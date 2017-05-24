bd90edd6c26c9672ceb52368a4e45659c321403c
Dominic Brown and Tyler Stephenson <dominic@>
2017-05-24 16:40:56 -0700

    add theme change option

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,13 +1,12 @@
 var apiKey = require('./../.env').apiKey;
 var mapsapi = require('google-maps-api')( apiKey );
 
-$(document).ready(function(){
-
+function myInitMap(selectedTheme) {
   mapsapi().then( function( maps ) {
     map = new google.maps.Map($('#map')[0], {
       center: {lat: 45.5231, lng: -122.6765},
       zoom: 8,
-      styles: google_styles1
+      styles: selectedTheme
     });
     google.maps.event.addListener(map, 'click', function(e) {
       // placeMarker(e.latLng, map);
@@ -15,13 +14,44 @@ $(document).ready(function(){
       var myLng = e.latLng.lng();
       var infowindow = new google.maps.InfoWindow({
         position: e.latLng,
-        content: "<p>Lattitude: " + myLat + "</p>" +
-            "<p>Longitude: " + myLng + "</p>"
+        content: "<h4>Lattitude: " + myLat.toFixed(3) + "</h4>" +
+            "<h4>Longitude: " + myLng.toFixed(3)  + "</h4>"
       });
       infowindow.open(map);
     });
   });
+}
+
+$(document).ready(function(){
+
+  var userTheme = google_styles_Vintage;
+
+  $('.theme').change(function() {
+    var theme = $('.theme :selected').val();
+    switch(theme) {
+      case "Vintage":
+          userTheme = google_styles_Vintage;
+          break;
+      case "AnnexPOI":
+          userTheme = google_styles_AnnexPOI;
+          break;
+      case "lucernewaterch1":
+          userTheme = google_styles_lucernewaterch1;
+          break;
+      case "Belgium":
+          userTheme = google_styles_Belgium;
+          break;
+      default:
+        "nothing"
+        break;
+    }
+    myInitMap(userTheme);
+  });
+
+  myInitMap(userTheme);
+
 });
+
 
 // NOTES
 // function placeMarker(position, map) {
